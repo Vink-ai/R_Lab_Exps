6de6b3b7aea12cce48efa378d1a53e682a156eb8
=Snowlag <=ankitjoshicr7@gmail.com>
2022-04-27 00:23:44 +0530

    added partial solution

--- exp2.R
+++ exp2.R
@@ -1,3 +1,13 @@
 cereals <- read.csv("Cereals.csv")
 str(cereals)
 head(cereals)
+view(cereals)
+x <- cereals[,  c(5 , 6 , 7)]
+head(x)
+min(x)
+max(x)
+length(x)
+var(x)
+# returns NA need to fix.
+mean(x)
+
